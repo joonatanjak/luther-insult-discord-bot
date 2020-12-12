dec426c37900a13fb530a3215af5d41efde8cbca
Joonatan Jakobson <joonatanjak@gmail.com>
2020-12-12 23:06:12 +0200

    Repository creation

---
+++ README.md
@@ -0,0 +1,13 @@
+# Luther Insult Discord Bot
+
+Luther Insult Discord Bot is a Discord bot that insult you with an insult from Martin Luther. The insults have been taken from [Lutheran Insulter](https://ergofabulous.org/luther/).
+
+## Installation
+You must have `git` and `node` installed.
+
+1. Clone the repository.
+    `git clone https://github.com/joonatanjak/luther-insult-discord-bot.git`
+2. Install the requirements.
+    `npm install luther-insult-djsckrd-bot/`
+3. Run the bot with your Discord Bot API token.
+    `BOT_KEY='YOUR-TOKEN-HERE' node luther-insult-discord-bot`
---
+++ index.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const insults = require('./insults.json');
+const prefix = '/';
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setActivity('/insult [user]', { type: 'LISTENING' });
+});
+
+function insult(channel, target){
+    let randomInsult = insults[Math.floor(Math.random() * insults.length)];
+    channel.send(`<@${target.id}>, ${randomInsult}`);
+}
+
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+    
+	if (command === 'insult') {
+        if(message.mentions.members.first()) insult(message.channel, message.mentions.members.first());
+        else if (args[0]) message.guild.members.fetch({ query: args[0], limit: 1 }).then(members => insult(message.channel, members.first().user)).catch(console.error);
+        else insult(message.channel, message.author);
+
+    }
+    
+});
+
+client.login(process.env.BOT_KEY);
